c12e5bc8a270fc51ff0966d36ac009a6f289eb0e
Chris Bourke <cbourke@cse.unl.edu>
2020-07-09 11:12:57 -0500

    style cleanup

--- src/unl/cse/recursion/Palindrome.java
+++ src/unl/cse/recursion/Palindrome.java
@@ -1,47 +1,45 @@
 package unl.cse.recursion;
 
 public class Palindrome {
-    
-    public static void main( String[] args ){
-        
-        String inputWord;
-        
-        if( args.length != 1 ){
-            
-            System.out.println( "Usage: isPalindrome word." );
-            System.exit( 1 );
-            
-        }
 
-        inputWord = args[0];
-        
-        /* Find out if it's a palindrome */
-        boolean isPal = isPalindrome( inputWord, 0, (inputWord.length() - 1) );
-        
-        if( isPal ){
-            System.out.format( "%s is a palindrome!\n", inputWord );
-        }
-        else
-            System.out.format( "%s is not a palindrome.\n", inputWord );
-        
+	public static void main(String[] args) {
 
-    } 
-    
-    /**
-     * Determines if a provided word or phrase is a palindrome or not.
-     * @param word String reference for the word or phrase to be tested.
-     * @param leftIndex The leftmost index of the word or phrase.
-     * @param rightIndex The rightmost index of the word or phrase.
-     * @return Boolean true if the word or phrase is a palindrome, false otherwise.
-     */
-    private static boolean isPalindrome( String word, int leftIndex, int rightIndex ){
-        
-        // Implement this method to determine if the String passed in is a 
-        // palindrome or not.
-        
-        return false;
-        
-    } 
+		String inputWord;
 
+		if (args.length != 1) {
+
+			System.out.println("Usage: isPalindrome word.");
+			System.exit(1);
+
+		}
+
+		inputWord = args[0];
+
+		/* Find out if it's a palindrome */
+		boolean isPal = isPalindrome(inputWord, 0, (inputWord.length() - 1));
+
+		if (isPal) {
+			System.out.format("%s is a palindrome!\n", inputWord);
+		} else {
+			System.out.format("%s is not a palindrome.\n", inputWord);
+		}
+	}
+
+	/**
+	 * Determines if a provided word or phrase is a palindrome or not.
+	 * 
+	 * @param word       String reference for the word or phrase to be tested.
+	 * @param leftIndex  The leftmost index of the word or phrase.
+	 * @param rightIndex The rightmost index of the word or phrase.
+	 * @return Boolean true if the word or phrase is a palindrome, false otherwise.
+	 */
+	private static boolean isPalindrome(String word, int leftIndex, int rightIndex) {
+
+		// Implement this method to determine if the String passed in is a
+		// palindrome or not.
+
+		return false;
+
+	}
 
 }
